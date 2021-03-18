4322b1bdb4c33c83448492ca409659cd8daf159e
Rigalkumar Sanjaybhai Patel <regalpatel273@users.noreply.github.com>
2021-03-18 22:13:12 +0530

    updated code

--- js/description.js
+++ js/description.js
@@ -1,8 +1,39 @@
-$(document).ready(function(){
-    $("#slider").bxSlider({
-        minSlides: 2,
-        maxSlides: 2,
-        slideWidth: 250,
-        slideMargin: 10
-    });
-});
+// This webpage is created by: Rigalkumar Patel (C0796389) 
+"use strict";
+class Succulents{
+    constructor(
+        plantName,
+        price,
+        type,
+        count
+    ){
+        this.plantName = plantName;
+        this.price = price;
+        this.type = type;
+        this.count = count;
+    }
+}
+var $ = (id) => {
+    return document.getElementById(id);
+}
+var succulent = new Succulents;
+succulent.plantName = "Zebra Plant";
+succulent.price = 14.99;
+succulent.type = "Indoor";
+succulent.count = 0;
+var count = 0;
+var addToCart = () => {
+    if(count === 0){
+        succulent.count = prompt("Enter number of items:");
+        $("atctick").innerHTML = `<img src="images/tick mark.gif" alt="tick mark" id="atcimg" height="25px" width="25px">`;
+        count +=1;
+    }
+    else{
+        alert("Item already added in the cart");
+    }
+}
+window.onload = () => {
+    $("atcbutton").onclick = addToCart;
+    $("atcbutton").focus();
+}
+export default succulent;
